39c45729572669806861eafac9db79ffbd7eb0c9
Default user <jf.rodriguezh@uniandes.edu.co>
2019-10-29 21:38:23 +0000

    Correcciones y primer a version del if

--- programa_2.cpp
+++ programa_2.cpp
@@ -8,6 +8,9 @@ int main()
     int c;
     std::cout<<"Escriba los tres lados del triangulo: \n";
     std::cin>>a>>b>>c;
-    
-    
+    if(a*a+b*b==c*c)
+    {
+        std::cout<<"El triangulo es rectangulo."<<std::endl;
+    }
+    return 0;
 }
