dbd3be78e8a165b3526b442d63c9f6154ea3be92
Default user <jf.rodriguezh@uniandes.edu.co>
2019-10-29 21:42:08 +0000

    El if tiene en cuenta la posibilidad de un input desordenado, se agrega segundo mensaje de output

--- programa_2.cpp
+++ programa_2.cpp
@@ -8,9 +8,13 @@ int main()
     int c;
     std::cout<<"Escriba los tres lados del triangulo: \n";
     std::cin>>a>>b>>c;
-    if(a*a+b*b==c*c)
+    if((a*a+b*b==c*c)||(a*a+c*c==b*b)||(b*b+c*c==a*a))
     {
         std::cout<<"El triangulo es rectangulo."<<std::endl;
+    }
+    else
+    {
+        std::cout<<"El triangulo no es rectangulo."<<std::endl;
     }
     return 0;
 }
