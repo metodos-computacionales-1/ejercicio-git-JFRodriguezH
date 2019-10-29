dd4984f8a9263a5b467b867dd6d9cc8aa0ca63b8
Default user <jf.rodriguezh@uniandes.edu.co>
2019-10-29 21:18:20 +0000

    Primera familarizacion con c++

---
+++ programa_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+    int N=100;
+    int a=0;
+    for(int i=0; i<=N; i++)
+    {
+        a+=i;
+        std::cout<<a<<std::endl;
+    }
+    
+    return 0;
+}
