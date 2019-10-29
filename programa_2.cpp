f24a23f3791109ca0d45ec02b92b0c0424e88855
Default user <jf.rodriguezh@uniandes.edu.co>
2019-10-29 21:34:20 +0000

    Se crea el programa, se piden los tres lados

---
+++ programa_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    std::cout<<"Escriba los tres lados del triangulo: \n";
+    std::cin>>a>>b>>c;
+    
+    
+}
