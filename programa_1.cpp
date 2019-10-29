3a7f3c9aa9f7f002100569b74e48c82e4ed72377
Default user <jf.rodriguezh@uniandes.edu.co>
2019-10-29 21:26:20 +0000

    El programa devuelve el perimetro

--- programa_1.cpp
+++ programa_1.cpp
@@ -8,6 +8,7 @@ int main()
     std::cin>>R;
     float pi=2.0*asin(1.0);
     std::cout<<"El diametro es: "<<2.0*R<<std::endl;
+    std::cout<<"El perimetro es: "<<2.0*pi*R<<std::endl;
     
     return 0;
 }
